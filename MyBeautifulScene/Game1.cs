7a9551c4808322a68cc074915a1646f81462f3a9
CriticalDesign <aaron.langille@gmail.com>
2025-11-11 10:26:40 -0500

    Updated with controller support and some other fun stuff!!

--- MyBeautifulScene/Game1.cs
+++ MyBeautifulScene/Game1.cs
@@ -16,6 +16,9 @@ namespace MyBeautifulScene
         private Vector2 _coin1Position, _coin2Position, _coin3Position, _coin4Position, _coin5Position, 
                         _coin6Position, _coin7Position, _coin8Position, _coin9Position, _coin10Position;
 
+        private Vector2 _heroPosition;
+
+        private float _heroHSpeed, _heroVSpeed;
 
         private SpriteFont _gameFont;
         private Color _textColor;
@@ -46,6 +49,9 @@ namespace MyBeautifulScene
 
             _textColor = new Color(_rng.Next(128, 256), _rng.Next(128, 256), _rng.Next(128, 256));
 
+            _heroPosition = new Vector2(200, 300);
+            _heroHSpeed = 0f;
+            _heroVSpeed = 0f;
 
             base.Initialize();
         }
@@ -68,8 +74,85 @@ namespace MyBeautifulScene
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            //_textColor = new Color(_rng.Next(0, 256), _rng.Next(0, 256), _rng.Next(0, 256));
-            // TODO: Add your update logic here
+
+            _heroHSpeed = 0f;
+            _heroVSpeed = 0f;
+
+
+            _heroHSpeed = 2.5f * GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X;
+            _heroVSpeed = -2.5f * GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.Right)) {
+                _heroHSpeed = 2.5f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A) || Keyboard.GetState().IsKeyDown(Keys.Left)) {
+                _heroHSpeed = -2.5f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W) || Keyboard.GetState().IsKeyDown(Keys.Up)) {
+                _heroVSpeed = -2.5f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S) || Keyboard.GetState().IsKeyDown(Keys.Down)) {
+                _heroVSpeed = 2.5f;
+            }
+
+            if(GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.F))
+            {
+                _heroHSpeed *= 3f;
+                _heroVSpeed *= 3f;
+            }
+
+            _heroPosition += new Vector2(_heroHSpeed, _heroVSpeed);
+
+
+            if (_heroPosition.X > 700) {
+                _heroPosition.X = 700;
+            }
+
+            if (_heroPosition.X < 50) {
+                _heroPosition.X = 50;
+            }
+
+            if (_heroPosition.Y < 50) {
+                _heroPosition.Y = 50;
+            }
+
+            if (_heroPosition.Y > 370) { 
+                _heroPosition.Y = 370;
+            }
+
+
+
+
+            /*
+            //_heroPosition.X += 1f;
+            //_heroPosition.Y -= 0.1f;
+            _heroPosition += new Vector2(_heroHSpeed, _heroVSpeed);
+
+            if(_heroPosition.X > 700)  //_graphics.PreferredBackBufferWidth
+            {
+                _heroHSpeed = -_heroHSpeed;
+                _heroPosition.X = 700;
+            }
+
+            if (_heroPosition.X < 50)  
+            {
+                _heroHSpeed = -_heroHSpeed;
+                _heroPosition.X = 50;
+            }
+
+            if (_heroPosition.Y < 50)  
+            {
+                _heroVSpeed = -_heroVSpeed;
+                _heroPosition.Y = 50;
+            }
+
+            if (_heroPosition.Y > 370)  //_graphics.PreferredBackBufferHeight
+            {
+                _heroVSpeed = -_heroVSpeed;
+                _heroPosition.Y = 370;
+            }
+            */
+
 
             base.Update(gameTime);
         }
@@ -82,7 +165,38 @@ namespace MyBeautifulScene
             
             
             _spriteBatch.Draw(_shipBackgroundSprite, new Vector2(0,0), Color.White);
-            _spriteBatch.Draw(_heroSprite, new Vector2(1, 1), Color.White);
+
+
+
+            SpriteEffects heroFacing = SpriteEffects.None;
+            if (_heroHSpeed < 0)
+                heroFacing = SpriteEffects.FlipHorizontally;
+
+            _spriteBatch.Draw(
+                _heroSprite,        //sprite
+                _heroPosition,      //position
+                null,               //sub section rectangle from a spritesheet
+                Color.White,        //color
+                0f,                 //rotation
+                new Vector2(0,0),   //origin
+                1f,                 //scale
+                heroFacing, //effects
+                0f                  //depth
+                );
+
+
+            _spriteBatch.Draw(
+                _crateSprite,        //sprite
+                new Vector2(500, 320),      //position
+                null,               //sub section rectangle from a spritesheet
+                Color.White,        //color
+                3.14f/2f,                 //rotation
+                new Vector2(0, 0),   //origin
+                1.5f,                 //scale
+                heroFacing, //effects
+                0f                  //depth
+                );
+
             _spriteBatch.Draw(_crateSprite, new Vector2(100, 100), Color.White);
             
             _spriteBatch.Draw(_coinSprite, _coin1Position, Color.White);
